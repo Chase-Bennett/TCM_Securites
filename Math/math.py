0c9342c9ed6260a29d36ebac4998b3ca50a3ed60
Chase Bennett <bennettchase638@gmail.com>
2024-01-25 09:04:18 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,27 @@
+#!/bin/python3 
+
+  
+
+#Programer: Chase Bennett 
+
+#Program: math
+
+
+
+
+print(9 * 9)
+print(50 + 50) #Addition 
+print(50 - 50) #subtract
+print(50 * 50) #Multiply
+print(50 / 50) #Divide
+
+print(50 + 50 - 50 * 50 / 50) #PEMDAS
+
+print(50**3) #Exponets
+
+print(50 % 6) #Modulo
+print(50 / 6) #Division with remainder (float)
+
+print(50 // 6) # no remainder l
+
+

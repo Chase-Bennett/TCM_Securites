fc1c5e808a87a5b65996385127af3d01170d2197
Chase Bennett <bennettchase638@gmail.com>
2024-01-25 09:01:25 -0500

    Strings

---
+++ Strings/First.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+#Programer: Chase Bennett
+#Program: Strings
+
+print("Hello, world\n")
+
+print("7 + 8 =",7+8) 
+print("")
+print("9 + 3 = "+ str(9+3),"\n") 
+
+
+
+print("\n\n\n\n","""Happy new year!
+2024""") # Triple quote to print on multiple lines
+
+
+
+
